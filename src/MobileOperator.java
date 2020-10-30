eff7fe549d851d8c50ff1393fdbd7dece6611cf7
alfiiasharipova <alfiiasharipova@gmail.com>
2020-10-30 23:53:06 +0300

    Variables deleted

--- src/MobileOperator.java
+++ src/MobileOperator.java
@@ -2,11 +2,9 @@ public class MobileOperator {
     public static void main(String[] args) {
         int account = 100;
         int toppingUp = 1100;
-        int bonusMoney = 100;
-        int bonusValue = 1;
         int finalBonus =0;
         if (toppingUp > 1000) {
-            finalBonus = bonusMoney != 0 ? bonusValue * toppingUp / bonusMoney : 0;
+            finalBonus = toppingUp / 100;
         }
         account+=toppingUp+finalBonus;
         System.out.println("account: " + account);
