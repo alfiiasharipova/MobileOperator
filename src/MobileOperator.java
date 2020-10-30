73158dc7407b29bdd3b9287d5bae58445ab8c502
alfiiasharipova <alfiiasharipova@gmail.com>
2020-10-30 23:55:18 +0300

    Refactoring

--- src/MobileOperator.java
+++ src/MobileOperator.java
@@ -2,10 +2,7 @@ public class MobileOperator {
     public static void main(String[] args) {
         int account = 100;
         int toppingUp = 1100;
-        int finalBonus =0;
-        if (toppingUp > 1000) {
-            finalBonus = toppingUp / 100;
-        }
+        int finalBonus = toppingUp > 1000? toppingUp / 100:0;
         account+=toppingUp+finalBonus;
         System.out.println("account: " + account);
         System.out.println("final bonus: " + finalBonus);
